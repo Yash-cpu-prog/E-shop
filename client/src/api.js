60fdd357ad3fea2ddf5e147070ece4f5ecd7a121
Yash Kalamkar <yashkalamkar07@gmail.com>
2026-04-17 15:25:10 +0530

    fix api for production

--- client/src/api.js
+++ client/src/api.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-
-  console.log("SENDING TOKEN:", token); // 🔥 check
 
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
